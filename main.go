aa396a37d4eaca2b2d321e258c9baadd46eacdb1
hojun121 <hojun121@naver.com>
2022-12-17 15:47:38 +0900

    double linked list

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type PhoneBook struct {
+	Name   string
+	Number int
+	Pre    *PhoneBook
+	Next   *PhoneBook
+}
+
+func newPhoneBook(name string, number int, pre *PhoneBook, next *PhoneBook) PhoneBook {
+	return PhoneBook{
+		Name:   name,
+		Number: number,
+		Pre:    pre,
+		Next:   next,
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+	init := newPhoneBook("pjh", 0, nil, nil)
+	one := newPhoneBook("pjh1", 1, &init, nil)
+	init.Next = &one
+	two := newPhoneBook("pjh1", 2, &one, nil)
+	one.Next = &two
+	three := newPhoneBook("pjh1", 3, &two, nil)
+	two.Next = &three
+	curPhone := &init
+	for {
+		fmt.Println(curPhone.Name)
+		fmt.Println(curPhone.Number)
+		if curPhone.Next == nil {
+			break
+		}
+		curPhone = curPhone.Next
+	}
 }
